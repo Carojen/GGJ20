4fb9afce632982791969845aaa3228ffbb691b6e
Carojen <Carojen@users.noreply.github.com>
2020-02-01 11:33:33 +0100

    Add Character. Saved Main scene.

---
+++ Flaskpost/Assets/_Scripts/CharacterSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterSettings", menuName = "ScriptableObjects/CharacterSettings", order = 1)]
+public class CharacterSettings : ScriptableObject
+{
+    
+}
