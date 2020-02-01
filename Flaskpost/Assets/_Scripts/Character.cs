01ffc2b2d59648912f0d3424ef2683c7b444bfc6
Carojen <Carojen@users.noreply.github.com>
2020-02-01 12:23:53 +0100

    Add character bounce.

--- Flaskpost/Assets/_Scripts/Character.cs
+++ Flaskpost/Assets/_Scripts/Character.cs
@@ -7,15 +7,24 @@ namespace Flaskpost
         [SerializeField]
         private CharacterSettings m_CharacterSettings = null;
 
+        [SerializeField]
+        private Rigidbody m_Rigidbody = null;
+
+
         private void Update()
         {
-
+            if(Input.GetKeyDown(KeyCode.Space))
+                m_Rigidbody.AddForce(Vector3.Reflect(-transform.up, transform.up * m_CharacterSettings.BouncePower * 0.5f),
+                               ForceMode.Impulse);
         }
 
-        private void Bounce()
-        {
-
+        void OnCollisionEnter(Collision collision)
+        {            
+            var direction = m_Rigidbody.velocity.normalized;
+            var magnitude = m_Rigidbody.velocity.magnitude;
+            Debug.Log("Collision " + (magnitude * m_CharacterSettings.BouncePower).ToString());
+            m_Rigidbody.AddForce((Vector3.Reflect(direction, collision.contacts[0].normal) 
+                * magnitude * m_CharacterSettings.BouncePower) - Physics.gravity, ForceMode.Impulse);
         }
-
     }
 }
--- Flaskpost/Assets/_Scripts/CharacterSettings.cs
+++ Flaskpost/Assets/_Scripts/CharacterSettings.cs
@@ -5,6 +5,20 @@ namespace Flaskpost
     [CreateAssetMenu(fileName = "CharacterSettings", menuName = "ScriptableObjects/CharacterSettings", order = 1)]
     public class CharacterSettings : ScriptableObject
     {
+        //[SerializeField]
+        [Range(0f, 10f)]
+        private float m_BouncePower = 1f;
+        public float BouncePower { get { return m_BouncePower; } }
 
+        [SerializeField]
+        [Range(0f, 5f)]
+        private float m_Gravity = 1f;
+        
+        public void OnValidate()
+        {            
+            var gravity = Physics.gravity;
+            gravity.y = -m_Gravity * 9f;
+            Physics.gravity = gravity;
+        }
     }
 }
