4443b55e582cbca9ab17dc5b27c211930fdd127a
Elin Hallne <elin.hallne@hotmail.com>
2020-02-01 20:44:18 +0100

    LevelDesign
    Conecting tiles

---
+++ Flaskpost/Assets/_Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform playerObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookOnObject = playerObject.position - transform.position;
+        lookOnObject = playerObject.position - transform.position;
+        transform.forward = lookOnObject.normalized;
+    }
+}
