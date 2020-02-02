f98fba0b3009a2fdfa020592fae6be7f314badcd
Solvej von Malmborg <50319675+Vitterhorn@users.noreply.github.com>
2020-02-02 13:16:35 +0100

    Menyljud

---
+++ Flaskpost/Assets/_Scripts/MainButtonAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class MainButtonAudio : MonoBehaviour
+{
+	public StudioEventEmitter audioEmitter;
+
+    // Update is called once per frame
+    public void ClickSound()
+    {
+        audioEmitter.Play ();
+    }
+}
